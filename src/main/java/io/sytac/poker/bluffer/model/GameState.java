9059f9bf06428995d87cc382d9b16981ef14020a
Paizo <paizo.gazasoft@gmail.com>
2025-07-04 14:33:08 +0200

    port 8081, solved mapping issues on booleans

--- src/main/java/io/sytac/poker/bluffer/model/GameState.java
+++ src/main/java/io/sytac/poker/bluffer/model/GameState.java
@@ -2,11 +2,13 @@ package io.sytac.poker.bluffer.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GameState {
     private List<Player> players;
     private List<Card> communityCards;
--- src/main/java/io/sytac/poker/bluffer/model/Player.java
+++ src/main/java/io/sytac/poker/bluffer/model/Player.java
@@ -1,18 +1,25 @@
 package io.sytac.poker.bluffer.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Player {
     private String id;
     private String name;
     private int chips;
     private String status;
     private int currentBet;
+    @JsonProperty("isDealer")
     private boolean isDealer;
+    @JsonProperty("isSmallBlind")
     private boolean isSmallBlind;
+    @JsonProperty("isBigBlind")
     private boolean isBigBlind;
+    @JsonProperty("isSpectator")
     private boolean isSpectator;
 }
